fb8bde2ca87d1b03746f6e7bd2212e171fc16824
MLTurner <mlturne3@ncsu.edu>
2022-07-03 23:11:38 -0400

    pulling stashed git branch

--- src/App.js
+++ src/App.js
@@ -1,39 +1,62 @@
+import { Fragment, useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Gallery from "./components/Gallery";
+import SearchBar from "./components/SearchBar";
+import AlbumView from "./components/AlbumView";
+import ArtistView from "./components/ArtistView";
 
+function App() {
+  let [search, setSearch] = useState("");
+  let [message, setMessage] = useState("Search for Music!");
+  let [data, setData] = useState([]);
 
+  const API_URL = "https://itunes.apple.com/search?term=";
 
-import { useEffect, useState } from 'react'
-import Gallery from './components/Gallery'
-import SearchBar from './components/SearchBar'
-
-function App(){
-    let [search, setSearch] = useState('')
-    let [message, setMessage] = useState('Search for Music!')
-    let [data, setData] = useState([])
-
-    useEffect(() => {
-      const fetchData = async () => {
-        document.title = `${search} Music`
-        const response = await fetch ('https://itunes.apple.com/search?term=the%20grateful%20dead')
-        const resData = await response.json()
-        if (resData.results.length > 0) {
-        setData(resData.results)
-        } else {
-        setMessage('Not Found')
-        }
+  useEffect(() => {
+    const fetchData = async () => {
+      document.title = `${search} Music`;
+      const response = await fetch(
+        API_URL + term
+      );
+      const resData = await response.json();
+      if (resData.results.length > 0) {
+        setData(resData.results);
+      } else {
+        setMessage("Not Found");
       }
-    fetchData()
-}, [search])
+    };
+    fetchData();
+  }, [search]);
 
-    return (
-        <div>
-            <SearchBar />
-            {message}
-            <Gallery />
-        </div>
-    )
+  const handleSearch = (e, term) => {
+    e.preventDefault();
+    setSearch(term);
+  };
+
+  return (
+    <div>
+      {message}
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Fragment>
+                <SearchBar handleSearch={handleSearch} />
+                <Gallery data={data} />
+              </Fragment>
+            }
+          />
+
+       <Route path="/album/:id" element={<AlbumView />} />
+       <Route path="/artist/:id" element={<ArtistView />} />
+       </Routes>
+      </Router>
+    </div>
+  );
 }
 
-export default App
+export default App;
 
 /*import logo from './logo.svg';
 import './App.css';
